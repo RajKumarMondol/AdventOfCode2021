9f2194036b8de2b4f265f5b51555846e461978e2
Raj Kumar Mondol <rajkumar.mondol@tesco.com>
2021-12-07 20:55:45 +0530

    Day 1 Part 2: sonarSweepDepthIncrease with Window

--- day1.go
+++ day1.go
@@ -3,12 +3,26 @@ package main
 import "fmt"
 
 func day1(inputFileName string) {
-	fmt.Println("Day 01 : Problem 1 , Input : %s", inputFileName)
-	values := readIntArray(inputFileName)
-	fmt.Println("Input :")
-	printArray(values)
-	result := sonarSweepDepthIncrease(values)
-	fmt.Println("Day 01 : Result : ", result)
+	fmt.Println("Day 01 : Problem 1 : Input : ", inputFileName)
+	sonarData := readIntArray(inputFileName)
+	// fmt.Println("Input :")
+	// printArray(sonarData)
+	part1Result := sonarSweepDepthIncrease(sonarData)
+	fmt.Println("Day 01 : Problem 1 : Result : ", part1Result)
+	sonarDataSlidingWindow := windowSum(sonarData)
+	// fmt.Println("Sliding window depth:")
+	// printArray(sonarDataSlidingWindow)
+	part2Result := sonarSweepDepthIncrease(sonarDataSlidingWindow)
+	fmt.Println("Day 01 : Problem 2 : Result : ", part2Result)
+}
+func windowSum(values []int) []int {
+	windowData := []int{}
+	for index, _ := range values {
+		if index >= 2 {
+			windowData = append(windowData, (values[index] + values[index-1] + values[index-2]))
+		}
+	}
+	return windowData
 }
 func sonarSweepDepthIncrease(values []int) int {
 	previousValue := 0
